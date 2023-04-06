b18a1a865d066a09817c679c6af3ee949221fd64
akmaghakian <129209752+akmaghakian@users.noreply.github.com>
2023-04-06 04:48:58 +0100

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,4 @@
+# We firstly indicate the range where we want to run the "*" sign limiting the amount to 5 of "*"
+for i in range(1, 6):
+    #We secondly indicate the character we want printed following the rule in line 2
+    print('*' * i)
